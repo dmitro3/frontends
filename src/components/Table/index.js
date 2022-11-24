c82beffdcdb5d946d01170d9842633ac3cf2d0ec
HoangYGG <hoanglai@yggsea.io>
2022-11-24 14:20:41 +0700

    update more coin

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -11,6 +11,8 @@ const Table = () => {
     btc: [],
     eth: [],
     matic: [],
+    avax: [],
+    usdc: [],
   });
 
   const [charts, setCharts] = useState({
@@ -26,6 +28,8 @@ const Table = () => {
         axios.get('https://api-v2.blockpour.com/api/prices/average/hourly/BTC?days=1'),
         axios.get('https://api-v2.blockpour.com/api/prices/average/hourly/MATIC?days=1'),
         axios.get('https://api-v2.blockpour.com/api/prices/average/hourly/ETH?days=1'),
+        axios.get('https://api-v2.blockpour.com/api/prices/average/hourly/AVAX?days=1'),
+        axios.get('https://api-v2.blockpour.com/api/prices/average/hourly/USDC?days=1'),
       ]);
 
       const responseFormatted = response.map((rs) => rs.data.data);
@@ -34,18 +38,24 @@ const Table = () => {
         btc: responseFormatted[0],
         matic: responseFormatted[1],
         eth: responseFormatted[2],
+        avax: responseFormatted[3],
+        usdc: responseFormatted[4],
       });
 
       setCharts({
         times: {
-          btc: responseFormatted[0].map((rs) => moment().unix(rs.time)),
-          matic: responseFormatted[1].map((rs) => moment().unix(rs.time)),
-          eth: responseFormatted[2].map((rs) => moment().unix(rs.time)),
+          btc: responseFormatted[0].map((rs) => moment(rs.time).toDate()),
+          matic: responseFormatted[1].map((rs) => moment(rs.time).toDate()),
+          eth: responseFormatted[2].map((rs) => moment(rs.time).toDate()),
+          avax: responseFormatted[3].map((rs) => moment(rs.time).toDate()),
+          usdc: responseFormatted[4].map((rs) => moment(rs.time).toDate()),
         },
         prices: {
           btc: responseFormatted[0].map((rs) => parseFloat(rs.price)),
           matic: responseFormatted[1].map((rs) => parseFloat(rs.price)),
           eth: responseFormatted[2].map((rs) => parseFloat(rs.price)),
+          avax: responseFormatted[3].map((rs) => parseFloat(rs.price)),
+          usdc: responseFormatted[4].map((rs) => parseFloat(rs.price)),
         },
       });
     };
@@ -58,6 +68,8 @@ const Table = () => {
       printChart(charts.times.btc, charts.prices.btc, 'btcRef');
       printChart(charts.times.eth, charts.prices.eth, 'ethRef');
       printChart(charts.times.matic, charts.prices.matic, 'maticRef');
+      printChart(charts.times.avax, charts.prices.avax, 'avaxRef');
+      printChart(charts.times.usdc, charts.prices.usdc, 'usdcRef');
 
       drawed.current = true;
     }
@@ -79,7 +91,9 @@ const Table = () => {
                 name: 'Volume',
                 isNumber: true,
               },
-
+              {
+                name: 'Weight',
+              },
               {
                 name: 'Last 7 Days',
                 isChart: true,
@@ -107,6 +121,7 @@ const Table = () => {
             <th className="thClass text-right">
               {prices.btc.length > 0 && currency(prices.btc[prices.btc.length - 1].volume).format()}
             </th>
+            <th className="thClass text-right w-5">25%</th>
             <th className="thClass relative">
               <canvas id="btcRef" />
             </th>
@@ -122,6 +137,7 @@ const Table = () => {
             <th className="thClass text-right">
               {prices.eth.length > 0 && currency(prices.eth[prices.eth.length - 1].volume).format()}
             </th>
+            <th className="thClass text-right w-5">25%</th>
             <th className="thClass relative">
               <canvas id="ethRef" />
             </th>
@@ -137,8 +153,41 @@ const Table = () => {
             <th className="thClass text-right">
               {prices.matic.length > 0 && currency(prices.matic[prices.matic.length - 1].volume).format()}
             </th>
+            <th className="thClass text-right w-5">25%</th>
             <th className="thClass relative">
               <canvas id="maticRef" />
+            </th>
+          </tr>
+          <tr className="border-b bg-gray-800 border-gray-700">
+            <th scope="row" className="thClass flex items-center">
+              <Image src="/avalanche-avax-logo.png" width={20} height={20} alt="polygon" />
+              <span className="ml-3">Avalanche</span>
+            </th>
+            <th className="thClass text-right">
+              {prices.avax.length > 0 && currency(prices.avax[prices.avax.length - 1].price).format()}
+            </th>
+            <th className="thClass text-right">
+              {prices.avax.length > 0 && currency(prices.avax[prices.avax.length - 1].volume).format()}
+            </th>
+            <th className="thClass text-right w-5">25%</th>
+            <th className="thClass relative">
+              <canvas id="avaxRef" />
+            </th>
+          </tr>
+          <tr className="border-b bg-gray-800 border-gray-700">
+            <th scope="row" className="thClass flex items-center">
+              <Image src="/usdc-logo.png" width={20} height={20} alt="polygon" />
+              <span className="ml-3">USDC</span>
+            </th>
+            <th className="thClass text-right">
+              {prices.usdc.length > 0 && currency(prices.usdc[prices.usdc.length - 1].price).format()}
+            </th>
+            <th className="thClass text-right">
+              {prices.usdc.length > 0 && currency(prices.usdc[prices.usdc.length - 1].volume).format()}
+            </th>
+            <th className="thClass text-right w-5">25%</th>
+            <th className="thClass relative">
+              <canvas id="usdcRef" />
             </th>
           </tr>
         </tbody>
