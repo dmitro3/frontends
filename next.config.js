c03cb0dff74cde42b4526076990f6f03fe8e2269
congtin <tincongphan@gmail.com>
2022-11-24 13:57:35 +0700

    update UI hoang

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
+  // images: {
+  //   loader: 'akamai',
+  //   path: '',
+  // },
   trailingSlash: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
